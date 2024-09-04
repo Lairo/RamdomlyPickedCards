7cf95a4605cd38016a5c4029a2735c6098770c21
chajus1 <chajus1@googlemail.com>
2024-09-04 23:32:07 +0100

    Add project files.

---
+++ RamdomlyPickedCards/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace math
+{
+    class BigMath
+    {
+        static void Main()
+        {
+            int height = 179;
+            int width = 83;
+            int area = Multiply(height, width);
+
+            int Multiply(int factorial1, int factorial2)
+            {
+                int product = factorial1 * factorial2;
+                return product;
+            }
+
+
+            Console.WriteLine(area);
+        }
+    }
+}
+
